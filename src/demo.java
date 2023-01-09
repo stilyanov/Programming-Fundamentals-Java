94c6ccd1e551a3ebe1878d77546b985e08fc0127
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-01-09 22:05:29 +0200

    test

--- src/demo.java
+++ src/demo.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello World!");
+
+        int age = Integer.parseInt(scanner.nextLine());
+        String name = scanner.nextLine();
+
+        System.out.printf("My name is %s and i am %d years old", name, age);
 
     }
 }
