c4acb3152aed7f029f8045489b96dd3caf97a04e
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-01-11 21:59:21 +0200

    upload

---
+++ src/Basic_Syntax_Conditional_Statements_And_Loops_LAB/Divisible_By_3_08.java
@@ -0,0 +1,13 @@
+package Basic_Syntax_Conditional_Statements_And_Loops_LAB;
+
+import java.util.Scanner;
+
+public class Divisible_By_3_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 3; i <= 100; i+= 3) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Basic_Syntax_Conditional_Statements_And_Loops_LAB/Multiplication_Table_10.java
@@ -0,0 +1,11 @@
+package Basic_Syntax_Conditional_Statements_And_Loops_LAB;
+
+import java.util.Scanner;
+
+public class Multiplication_Table_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
