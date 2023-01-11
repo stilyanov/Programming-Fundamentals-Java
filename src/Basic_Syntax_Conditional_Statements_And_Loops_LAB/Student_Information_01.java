99f4c53daf161f5f1036688681a51ce32984ca7b
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-01-11 20:42:44 +0200

    New Tasks

---
+++ src/Basic_Syntax_Conditional_Statements_And_Loops_LAB/Student_Information_01.java
@@ -0,0 +1,17 @@
+package Basic_Syntax_Conditional_Statements_And_Loops_LAB;
+
+import java.util.Scanner;
+
+public class Student_Information_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        String output = String.format("Name: %s, Age: %d, Grade: %.2f", name, age, grade);
+
+        System.out.println(output);
+    }
+}
---
+++ src/Basic_Syntax_Conditional_Statements_And_Loops_LAB/Theatre_Promotions_07.java
@@ -0,0 +1,11 @@
+package Basic_Syntax_Conditional_Statements_And_Loops_LAB;
+
+import java.util.Scanner;
+
+public class Theatre_Promotions_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
