e1e2fc10b7063c7b26cb78a83793221c3dd82e05
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-01-20 12:20:19 +0200

    new tasks

---
+++ src/DataTypeAndVariables/P01_MeterToKilometers.java
@@ -0,0 +1,15 @@
+package DataTypeAndVariables;
+
+import java.util.Scanner;
+
+public class P01_MeterToKilometers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        short meters = Short.parseShort(scanner.nextLine());
+
+        double kilometers = meters / 1000.0;
+
+        System.out.printf("%.2f", kilometers);
+    }
+}
---
+++ src/DataTypeAndVariables/P08_LowerOrUpper.java
@@ -0,0 +1,11 @@
+package DataTypeAndVariables;
+
+import java.util.Scanner;
+
+public class P08_LowerOrUpper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
