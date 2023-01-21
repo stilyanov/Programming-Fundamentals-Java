d1a5d5eb09b98a808631abb5bc93d59785cb1806
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-01-21 10:34:42 +0200

    uploading my tasks

--- src/DataTypeAndVariables/P08_LowerOrUpper.java
+++ src/DataTypeAndVariables/P08_LowerOrUpper.java
@@ -6,6 +6,12 @@ public class P08_LowerOrUpper {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        char letter = scanner.nextLine().charAt(0);
 
+        if (Character.isUpperCase(letter)) {
+            System.out.println("upper-case");
+        } else if (Character.isLowerCase(letter)) {
+            System.out.println("lower-case");
+        }
     }
 }
---
+++ src/DataTypeAndVariables/P11_RefactorVolumeOfPyramid.java
@@ -0,0 +1,25 @@
+package DataTypeAndVariables;
+
+import java.util.Scanner;
+
+public class P11_RefactorVolumeOfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double length, width, height = 0;
+
+
+        System.out.print("Length: ");
+        length = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("Width: ");
+        width = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("Height: ");
+        height = Double.parseDouble(scanner.nextLine());
+
+        double volume = (length * width * height) / 3;
+
+        System.out.printf("Pyramid Volume: %.2f", volume);
+    }
+}
