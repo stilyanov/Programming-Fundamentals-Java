a82e8bc21e576e203a679bb76592e8c8a0bc84f5
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-01-26 22:33:17 +0200

    Arrays

---
+++ src/Arrays_LAB/P06_EqualArrays.java
@@ -0,0 +1,36 @@
+package Arrays_LAB;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P06_EqualArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] firstArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] secondArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sum = 0;
+        boolean isEqual = false;
+        int difference = 0;
+
+        for (int i = 0; i < firstArr.length; i++) {
+            int firstArrElement = firstArr[i];
+            int secondArrElement = secondArr[i];
+
+            if (firstArrElement == secondArrElement) {
+                sum += firstArrElement;
+                isEqual = true;
+            } else {
+                isEqual = false;
+                difference = i;
+                break;
+            }
+        }
+        if (isEqual) {
+            System.out.printf("Arrays are identical. Sum: %d", sum);
+        } else {
+            System.out.printf("Arrays are not identical. Found difference at %d index.", difference);
+        }
+    }
+}
---
+++ src/Arrays_LAB/P07_CondenseArrayToNumber.java
@@ -0,0 +1,20 @@
+package Arrays_LAB;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P07_CondenseArrayToNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbersArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[] condensed = new int[numbersArr.length - 1];
+        int sum = 0;
+
+        for (int i = 0; i < numbersArr.length; i++) {
+            int currentElement = numbersArr[i];
+
+        }
+    }
+}
