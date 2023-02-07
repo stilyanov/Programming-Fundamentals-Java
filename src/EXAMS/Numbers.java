f5c926db57785bbd675f4ec263f5d1ef4081728d
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-02-07 22:42:23 +0200

    update

--- src/EXAMS/Numbers.java
+++ src/EXAMS/Numbers.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Numbers {
     public static void main(String[] args) {
