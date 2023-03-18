2d56c38df16b2183707157dc54566d38788f1709
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-03-18 16:58:09 +0200

    cheatSheet and regex

--- demo.java
+++ demo.java
@@ -4,21 +4,23 @@ public class demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        double heritage = Double.parseDouble(scanner.nextLine());
+        int years = Integer.parseInt(scanner.nextLine());
+        int age = 18;
+        for (int i = 1800; i <= years; i++) {
+            if (i % 2 == 0) {
+                heritage -= 12000;
+            } else {
+                heritage -= 12000 + (50 * age);
+            }
 
-        for (int i = 1; i <= n; i++) {
-            String text = scanner.nextLine();
-            int firstIndexName = text.lastIndexOf("@");
-            int lastIndexName = text.lastIndexOf("|");
-
-            String name = text.substring(firstIndexName + 1, lastIndexName);
-
-            int firstIndexAge = text.lastIndexOf("#");
-            int lastIndexAge = text.lastIndexOf("*");
-
-            String age = text.substring(firstIndexAge + 1, lastIndexAge);
-
-            System.out.println(name + " is " + age + " years old.");
+            age++;
+        }
+        if (heritage >= 0) {
+            System.out.printf("Yes! He will live a carefree life and will have %.2f dollars left.", heritage);
+        } else {
+            System.out.printf("He will need %.2f dollars to survive.", Math.abs(heritage));
         }
     }
 }
+
