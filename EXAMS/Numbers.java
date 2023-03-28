2a5e10a76da34472ea5dde08adc623b9d7ed3a39
stilyanov <122092690+stilyanov@users.noreply.github.com>
2023-03-28 23:36:53 +0300

    judge 100/100

--- EXAMS/Numbers.java
+++ EXAMS/Numbers.java
@@ -10,27 +10,45 @@ public class Numbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
         //10 20 30 40 50
-        double averageNumber = 1;
+
         int sum = 0;
         for (int number : numbers) {
             sum += number;
         }
-        averageNumber = sum * 1.0 / numbers.size();
-        Collections.sort(numbers);
-        Collections.reverse(numbers);
-        int count = 0;
+        double averageNumber = sum * 1.0 / numbers.size();
+
+        List<Integer> greaterElements = new ArrayList<>();
         for (int number : numbers) {
-            if (count == 5) {
-                break;
-            }
-            count++;
             if (number > averageNumber) {
-                System.out.print(number + " ");
-            } else if (averageNumber == 1) {
-                System.out.println("No");
+                greaterElements.add(number);
             }
+        }
+
+        Collections.sort(greaterElements);
+        Collections.reverse(greaterElements);
+
+        if (greaterElements.size() > 0) {
+            if (greaterElements.size() < 5) {
+
+                System.out.println(greaterElements.toString()
+                        .replace("[", "")
+                        .replace("]", "")
+                        .replace(",", "")
+                );
+
+            } else {
+
+                List<Integer> top5Numbers = greaterElements.subList(0, 5);
+                for (int number : top5Numbers) {
+                    System.out.print(number + " ");
+                }
+            }
+
+        } else {
+
+            System.out.println("No");
         }
     }
 }
